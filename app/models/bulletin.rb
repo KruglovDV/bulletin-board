f61c97d1c626ab0b5951bfa61f24b26751652937
dimakruglov <kruglovdmitrij@itclover.ru>
2022-08-24 16:04:49 +0300

    fix: linters

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Bulletin < ApplicationRecord
   belongs_to :category
   belongs_to :user
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Category.create(name: 'Автомобили')
 Category.create(name: 'Животные')
 Category.create(name: 'Спорт')
